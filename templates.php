e3296aa34c0c794b1f7e2db25a8a770fdc87a118
Ali Alaa <hello@alialaa.com>
2022-01-29 11:28:21 +0200

    Added templates

--- templates.php
+++ templates.php
@@ -2,8 +2,40 @@
 
 function blocks_course_plugin_register_template() {
     $post_type_object = get_post_type_object( 'post' );
+    $post_type_object->template_lock = 'insert';
     $post_type_object->template = array(
-        array('blocks-course/metadata-block')
+        array('blocks-course/metadata-block'),
+        array('core/paragraph', array('content' => 'Some text')),
+        array(
+            'blocks-course/team-members', 
+            array('columns' => 3), 
+            array(
+                array(
+                    'blocks-course/team-member',
+                    array(
+                        'name' => 'John Doe',
+                        'bio' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque nibh, tincidunt ut facilisis vitae, ullamcorper sit amet lectus. Proin porta vulputate purus.',
+                        'url' => 'https://picsum.photos/id/1012/300/200'
+                    )
+                ),
+                array(
+                    'blocks-course/team-member',
+                    array(
+                        'name' => 'John Doe',
+                        'bio' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque nibh, tincidunt ut facilisis vitae, ullamcorper sit amet lectus. Proin porta vulputate purus.',
+                        'url' => 'https://picsum.photos/id/1012/300/200'
+                    )
+                ),
+                array(
+                    'blocks-course/team-member',
+                    array(
+                        'name' => 'John Doe',
+                        'bio' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam neque nibh, tincidunt ut facilisis vitae, ullamcorper sit amet lectus. Proin porta vulputate purus.',
+                        'url' => 'https://picsum.photos/id/1012/300/200'
+                    )
+                )
+            )
+        )
     );
 }
 add_action( 'init', 'blocks_course_plugin_register_template' );
